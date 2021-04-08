c2576639db7ae8cf6d8ceb8e94c8e4576cc959d9
Robin-Lee03 <bnmgv283@gmail.com>
2021-04-08 23:09:14 +0800

    0408

--- Matplotlib/Matplotlib0329.py
+++ Matplotlib/Matplotlib0329.py
@@ -6,11 +6,11 @@ y1 = 2*x+1
 y2 = 2**x
 
 plt.figure(figsize=(9,5))               # 概念像是創造一個新的圖表 figure以下的程式就視為data
-plt.plot(x,y1,color='hotpink',linestyle = '-.')
+plt.plot(x,y1,color='hotpink',linestyle = '-.',label='up')
 
 # plt.figure()
-plt.plot(x,y2,color='lightblue',marker = 'o')
-
+plt.plot(x,y2,color='lightblue',marker = 'o',label='down')
+plt.legend(loc='best')  # 設定完label後 要讓它顯示出來
 plt.xlim((0,10))
 plt.ylim((0,40))
 plt.xlabel('I am x')      # 給予縱軸和橫軸'標示'
