2dd9067465f4b910bc6cf7081f53e93514d83011
Robin-Lee03 <bnmgv283@gmail.com>
2021-05-22 23:48:41 +0800

    MAP

--- Udemy-project2/Webmap_datasources/__MACOSX/Population_Map 0522.py
+++ Udemy-project2/Webmap_datasources/__MACOSX/Population_Map 0522.py
@@ -18,19 +18,24 @@ def color_produce(elevation):
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 
 for ln, lt, el in zip(lon, lat, elev):
 
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 6,popup=str(el)+"m",
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius = 6,popup=str(el)+"m",
     fill_color = color_produce(el),color = "black", fill = True , fill_opacity = 0.7))
 
-fg.add_child(folium.GeoJson(data=(open("world.json",'r',encoding='utf-8-sig').read()),
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=(open("world.json",'r',encoding='utf-8-sig').read()),
                             style_function = lambda x: {'fillColor':'yellow'
                             if x['properties']['POP2005']<10000000 else 'orange'
                             if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+# map.add_child(folium.LayerControl())    # doesn't work
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())      # need to under fg
 
 map.save("Population_Map.html")
