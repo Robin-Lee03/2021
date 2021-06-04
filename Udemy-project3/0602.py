30860b7f722e2ede7a363f6c34fb829e38f3dff4
Robin-Lee03 <bnmgv283@gmail.com>
2021-06-04 15:09:41 +0800

    Udemy-Project3

--- Udemy-project3/0602.py
+++ Udemy-project3/0602.py
@@ -1,9 +1,13 @@
 import justpy as jp
 
+'''
+    refer to : "highcharts docs" 、 "quasar style" those website  
+
+'''
 def app():
     wp = jp.QuasarPage()
     h1 = jp.QDiv(a=wp, text= 'Analysis of Course Review',
-                 classes='text-h3 text-center q-pa-md') # a= where it belong
+                 classes='text-h3 text-center q-pa-md')   # a= where it belong
     p1 = jp.QDiv(a=wp, text= 'These graphs represent course review analysis')
     return wp
 jp.justpy(app)
